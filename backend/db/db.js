5548e12e787e92a380ee99207034fe75d9acc341
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-01-20 15:13:02 +0530

    Initial commit - user signup and login

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+
+const connectToDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Connected to MongoDB');
+    }catch(err){
+        console.error('Error connecting to MongoDB',err);
+    }
+}
+
+module.exports=connectToDB;
