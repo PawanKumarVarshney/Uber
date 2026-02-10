4a66f1c56d55de1f0c3efe967cc59ffa1b6e31f4
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-02-10 22:40:25 +0530

    integrating user end points with react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-// import Start from './pages/Start'
+import Start from './pages/Start'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import Captainlogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
 import Home from './pages/Home'
-// import UserProtectWrapper from './pages/UserProtectWrapper'
-// import UserLogout from './pages/UserLogout'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
 // import CaptainHome from './pages/CaptainHome'
 // import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 // import CaptainLogout from './pages/CaptainLogout'
@@ -18,16 +18,25 @@ import Home from './pages/Home'
 const App = () => {
 
   return (
-    
-    <div>
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/login' element={<UserLogin />} />
-    <Route path='/signup' element={<UserSignup />} />
-    <Route path='/captain-login' element={<Captainlogin />} />
-    <Route path='/captain-signup' element={<CaptainSignup />} />
 
-    </Routes>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
+        <Route path='/user/logout'
+          element={<UserProtectWrapper>
+            <UserLogout />
+          </UserProtectWrapper>
+          } />
+      </Routes>
     </div>
   )
 }
