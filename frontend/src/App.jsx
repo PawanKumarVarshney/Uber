2f8efc874b24dc4b483cafd28e08e309f288013a
Pawan Kumar Varshney <pawanvarshney38@gmail.com>
2026-02-10 15:54:24 +0530

    user & captain signup and login page design

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+// import Start from './pages/Start'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
+// import UserProtectWrapper from './pages/UserProtectWrapper'
+// import UserLogout from './pages/UserLogout'
+// import CaptainHome from './pages/CaptainHome'
+// import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+// import CaptainLogout from './pages/CaptainLogout'
+// import Riding from './pages/Riding'
+// import CaptainRiding from './pages/CaptainRiding'
+// import 'remixicon/fonts/remixicon.css'
+
+const App = () => {
+
+  return (
+    
+    <div>
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<UserLogin />} />
+    <Route path='/signup' element={<UserSignup />} />
+    <Route path='/captain-login' element={<Captainlogin />} />
+    <Route path='/captain-signup' element={<CaptainSignup />} />
+
+    </Routes>
+    </div>
+  )
+}
+
+export default App
